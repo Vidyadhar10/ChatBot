a397a399161ec5b1616af8b63ac903022d4bab0f
Kakhekar <kakhekarshubham@gmail.com>
2023-04-20 18:28:03 +0530

    chat responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -319,3 +319,18 @@
   }
 
 })();
+
+
+jQuery(document).ready(function() {
+
+  $(".chat-list a").click(function() {
+      $(".chatbox").addClass('showbox');
+      return false;
+  });
+
+  $(".chat-icon").click(function() {
+      $(".chatbox").removeClass('showbox');
+  });
+
+
+});
