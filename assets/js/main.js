b1d0402a1d49e4d863ca578a16f40817da1d5648
subhan mullani <subhanmullani2161@gmail.com>
2023-04-20 11:54:21 +0530

    chat changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -319,3 +319,19 @@
   }
 
 })();
+
+// chat winddoe js
+
+jQuery(document).ready(function() {
+
+  $(".chat-list a").click(function() {
+      $(".chatbox").addClass('showbox');
+      return false;
+  });
+
+  $(".chat-icon").click(function() {
+      $(".chatbox").removeClass('showbox');
+  });
+
+
+});
